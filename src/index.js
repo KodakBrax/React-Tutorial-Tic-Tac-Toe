97994f6e855f1d90df2c68037c8337c0e267129f
Braxton White <white.3176@buckeyemail.osu.edu>
2022-01-06 19:55:33 -0500

    added two loops

--- src/index.js
+++ src/index.js
@@ -21,25 +21,17 @@ function Square(props) {
     }
   
     render() {
-  
+      const size = 3;
+      let squares = [];
+      for (let i = 0; i < size; i++) {
+        let row = [];
+        for (let j = 0; j < size; j++) {
+          row.push(this.renderSquare(i * size + j));
+        }
+        squares.push(<div className="board-row">{row}</div>);
+      }
       return (
-        <div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
+        <div>{squares}</div>
       );
     }
   }
